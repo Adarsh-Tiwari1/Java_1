40c012b8698be65f2c95ec4fbf9b8d7afcb82ca1
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-01 18:16:26 +0530

    Some Code

---
+++ Const.java
@@ -0,0 +1,56 @@
+/*
+class Const{
+    int i;
+    String s;
+    public static void main(String[] args){
+        Const c1 = new Const();
+        System.out.println(c1.i);
+        System.out.println(c1.s);
+    }
+}
+*/
+
+/*
+class Const{
+    int age;
+    String name;
+    float agee;
+    public static void main(String[] args){
+        Const c1 = new Const();
+        c1.age = 20;
+        c1.name = "Adarsh";
+        
+        c1.age = 50;
+        c1.name = "Adarsh Kumar Tiwari";
+        System.out.println(c1.age);
+        System.out.println(c1.name);
+        System.out.println(c1.agee);
+    }
+}
+*/
+
+/*
+class Const{
+    int age;
+    String name;
+    public static void main(String[] args){
+        Const c1 = new Const();
+        
+        System.out.println(c1.age = 'a');
+        System.out.println(c1.name = "Adarsh");        
+    }
+}
+*/
+
+/*
+class Const{
+    int age;
+    String name;
+    public static void main(String[] args){
+        Const c1 = new Const();
+        
+        System.out.println(c1.age = 20);
+        System.out.println(c1.name = "Adarsh Tiwar");      
+    }
+}
+*/
