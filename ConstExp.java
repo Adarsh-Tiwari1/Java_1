69133e1abb1d35eb9b238e3a13934993c992d02b
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-02 19:12:05 +0530

    Some Code

---
+++ ConstExp.java
@@ -0,0 +1,25 @@
+/*
+class ConstExp{
+    int i;   // This is Constructor : Constructor use initialize the value
+    String name;
+    public static void main(String[] args){
+        ConstExp c1 = new ConstExp();
+        c1.i = 100;
+        c1.name = "Adarsh Tiwari";
+        System.out.println(c1.i);
+        System.out.println(c1.name);
+    }
+}
+*/
+
+/*
+class ConstExp{
+    ConstExp(){ // Constructor name and class name should be same , nahi to error
+        System.out.println("No Argument Constructor");
+    }
+    public static void main(String[] args){
+        ConstExp c1 = new ConstExp();
+    }
+}
+*/
+
