2b4094e9ec650e3538cf0c54048b9d47c6b08fb6
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-02 19:05:59 +0530

    Some Code

---
+++ Const1.java
@@ -0,0 +1,41 @@
+/*
+class Const1{
+    Const1(){               // class name and constructor name must be same .
+        System.out.println("no arg constructor");
+    }
+    public static void main(String[] args){
+       Const1 c1 = new Const1();
+       
+    }
+}
+*/
+
+/*
+class Const1{
+    Const1(){
+        System.out.println("Const1 i am Const");
+    }
+    Const1(){  //  The code will not compile due to a constructor overloading issue.
+        System.out.println("Const1 i am Const2");
+    }
+    public static void main(String[] args){
+        Const1 h1 = new Const1();
+        Const1 h2 = new Const1();
+    }
+}
+*/
+
+/*
+class Const1{
+    Const1(){
+        System.out.println("Hello I Am Adarsh");
+    }
+    Const1(String LastName){
+        System.out.println("Hello I Am Adarsh " + LastName);
+    }
+    public static void main(String[] args){
+        Const1 c1 = new Const1();
+        Const1 c2 = new Const1("Tiwari");
+    }
+}
+*/
