9113ceae917cead7d7b3fcdde150ed5e14714978
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-02 19:06:47 +0530

    Constant Value

---
+++ Const2.java
@@ -0,0 +1,9 @@
+
+class Const2{
+    Const2(int x){ // paramiterize constructor
+        System.out.println(x);
+    }
+    public static void main(String[] args){
+        Const2 c1 = new Const2(10);
+    }
+}
