d2c0940899696b919f3799b171c96d0ae98b19a3
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-04 14:11:19 +0530

    Instance

---
+++ Inst1.java
@@ -0,0 +1,11 @@
+
+class Inst1{
+    String color;
+    int age;
+    public static void main(String[] args){
+        Inst1 i1 = new Inst1();
+        i1.color = "Red";
+        i1.age = 10;
+        System.out.println(i1.color);
+    }
+}
