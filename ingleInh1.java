a1ccbe2f6df292f22ee4b6c15331e1812f08f34c
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-06 19:49:33 +0530

    inheritance class

---
+++ ingleInh1.java
@@ -0,0 +1,90 @@
+/*
+class A{
+    void showA(){
+        System.out.println("A class method");
+    }
+}
+class ingleInh1 extends A{
+    void showingleInh1(){
+        System.out.println("b class method");
+    }
+    public static void main(String[] args){
+        A a1 = new A();
+        a1.showA();
+
+        ingleInh1 b1 = new ingleInh1();
+        b1.showingleInh1();
+        b1.showA(); // a method ke call kar sakte hai.
+    }
+}
+*/
+
+
+/*
+class Cow{
+    void Run(){
+        System.out.println("Cow is Eating");
+        System.out.println("Cow is Runnigt");
+    }
+}
+class Dog extends Cow{
+    void Walk(){
+        System.out.println("Dog is Wark");
+        System.out.println("Dog is a Pet Animal");
+    }
+}
+class ingleInh1{
+    public static void main(String[] args){
+        Cow c1 = new Cow();
+        c1.Run();
+
+        Dog d1 = new Dog();
+        d1.Walk();
+
+    }
+}
+*/
+
+
+/*
+class Ram{
+    void ram(){
+        System.out.println("Ram is my God ");
+    }
+}
+class Sita extends Ram{
+    void sita(){
+        System.out.println("Sita is my Mother Maa Lakshmi ");
+    }
+}
+class ingleInh1{
+    public static void main(String[] args){
+        Sita s1 = new Sita();
+        s1.ram();
+        s1.sita();
+    }
+}
+*/
+
+
+/*
+class Mob{
+    void Mo(){
+        System.out.println("Hello Mo");
+    }
+}
+class ingleInh1 extends Mob{
+    void Mai(){
+        System.out.println("Hii Mai");
+    }
+    public static void main(String[] args){
+        Mob m1 = new Mob();
+        m1.Mo();
+
+        ingleInh1 h1 = new ingleInh1();
+        h1.Mai();
+
+        h1.Mo();
+    }
+}
+*/
