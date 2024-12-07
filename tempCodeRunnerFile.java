ae22390f16b907344b156214172aefddcf0ac9a2
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-07 14:12:01 +0530

    Some Code

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+class ConstDurga{
+    ConstDurga(){
+        this(10);
+    }
+    ConstDurga(int i){
+        this();
+    }
+    public static void main(String[] args){
+        System.out.println("Hell0");
+    }
+}
